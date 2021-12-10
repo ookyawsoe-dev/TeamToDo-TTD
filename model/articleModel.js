00f2594019769dc62d99baa73cb3923003f515db
Oxy Thu <oxythu27@gmail.com>
2021-12-10 15:15:25 +0630

    some update added

--- model/articleModel.js
+++ model/articleModel.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql2');
 
 // create the connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'staff',
-    password: 'Staff!@#$%67890',
-    database: 'user',
-});
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'staff',
+//     password: 'Staff!@#$%67890',
+//     database: 'user',
+// });
 
 //connection testing
 connection.connect((error) => {
