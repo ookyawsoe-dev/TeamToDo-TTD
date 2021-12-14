504fd2244e0ac1ed125536fc424e4c87a9d17a48
Oxy Thu <oxythu27@gmail.com>
2021-12-14 12:52:27 +0630

    add article ui

--- controller/articleController.js
+++ controller/articleController.js
@@ -1,12 +1,12 @@
-const articleModel = require('../model/articleModel');
+// const articleModel = require('../model/articleModel');
 
-//  read article
-exports.readarticle = (req, res, next) => {
-    articleModel.readarticle((err, results) => {
-        if(err){
-            console.log(err);
-        }else{
-            res.render('readarticles', {data : results});
-        }
-    });
-};
+// //  read article
+// exports.readarticle = (req, res, next) => {
+//     articleModel.readarticle((err, results) => {
+//         if(err){
+//             console.log(err);
+//         }else{
+//             res.render('readarticles', {data : results});
+//         }
+//     });
+// };
--- model/articleModel.js
+++ model/articleModel.js
@@ -8,25 +8,25 @@ const mysql = require('mysql2');
 //     database: 'user',
 // });
 
-//connection testing
-connection.connect((error) => {
-    if (error) {
-        console.log("Connect Error", error);
-    } else {
-        console.log("The Database is Connected.");
-    }
-});
+// //connection testing
+// connection.connect((error) => {
+//     if (error) {
+//         console.log("Connect Error", error);
+//     } else {
+//         console.log("The Database is Connected.");
+//     }
+// });
 
-// read article
-exports.readarticle = (callback) => {
-    connection.query(
-        'SELECT article_id, article_name, about_article, creater_name, DATE_FORMAT(created_date, "%Y-%m-%d") as created_date FROM `article`',
-        (err, results) => {
-            if (err) {
-                console.log(err);
-            }
-            callback(err, results);
-        }
-    );
+// // read article
+// exports.readarticle = (callback) => {
+//     connection.query(
+//         'SELECT article_id, article_name, about_article, creater_name, DATE_FORMAT(created_date, "%Y-%m-%d") as created_date FROM `article`',
+//         (err, results) => {
+//             if (err) {
+//                 console.log(err);
+//             }
+//             callback(err, results);
+//         }
+//     );
 
-};
+// };
