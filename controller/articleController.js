13da4918865fc045f0ba4c5d303c234cdfd1fc01
ookyawsoe-ucss <ookyawsoe2000@gmail.com>
2021-12-14 16:28:48 +0630

    commit some updated

--- controller/articleController.js
+++ controller/articleController.js
@@ -1,12 +1,23 @@
-// const articleModel = require('../model/articleModel');
+const articleModel = require('../model/articleModel');
 
-// //  read article
-// exports.readarticle = (req, res, next) => {
-//     articleModel.readarticle((err, results) => {
-//         if(err){
-//             console.log(err);
-//         }else{
-//             res.render('readarticles', {data : results});
-//         }
-//     });
-// };
+//  read article
+exports.readarticle = (req, res, next) => {
+    articleModel.readarticle((err, results) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.render('readarticles', {data : results});
+        }
+    });
+};
+
+// add article
+exports.addarticle = (req, res, next) => {
+    articleModel.addarticle((err, results) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.render('newarticle', { data : results });
+        }
+    });
+}
--- routes/article.js
+++ routes/article.js
@@ -4,5 +4,6 @@ var router = express.Router();
 var articleController = require('../controller/articleController');
 /* GET users listing. */
 router.get('/', articleController.readarticle);
+router.get('/addarticle', articleController.addarticle);
 
 module.exports = router;
