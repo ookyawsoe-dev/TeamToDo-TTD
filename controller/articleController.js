b3c9c0daa13cef43e5f8e94d916312179a691e56
ookyawsoe-ucss <ookyawsoe2000@gmail.com>
2021-12-17 19:58:37 +0630

    commit article action  from oks

--- controller/articleController.js
+++ controller/articleController.js
@@ -2,6 +2,7 @@
 const articleModel = require('../model/articleModel');
 const commentModel = require('../model/commentModel');
 const auth = require('../Middleware/checkAuth');
+const { param } = require('../routes');
 
 
 
@@ -25,15 +26,15 @@ exports.readarticle = (req, res, next) => {
     })
 }
 
-// exports.readarticle = (req, res, next) => {
-//     articleModel.readarticle((err, results) => {
-//         if(err){
-//             console.log(err);
-//         }else{
-//             res.render('readarticles', {data : results});
-//         }
-//     });
-// };
+exports.showarticle = (req, res, next) => {
+    articleModel.showarticle((err, results) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.render('list_article', {data : results});
+        }
+    });
+};
 
 
 // add article
@@ -42,12 +43,14 @@ exports.addarticle = (req, res, next) => {
     if(req.method == "GET"){
         res.render('add_article',{layout : false});
     }else{
-        articleModel.addarticle(params, (err, results) => {
+        var id = req.session.user.user_id;
+        articleModel.addarticle(id,params, (err, results) => {
             if(err) {
                 console.log(err);
+                console.log("Aung Si Thu + Myo Chay");
             }else{
                 console.log("Testing Redirect");
-                res.redirect('/article');
+                res.redirect('/');
             }
         });
     }
@@ -55,17 +58,88 @@ exports.addarticle = (req, res, next) => {
 }
 
 // delete article
-exports.deletearticle = (req, res) => {
-    const article_id = req.params.article_id;
-    console.log("Method Name: ", req.method);
+// exports.deletearticle = (req, res) => {
+//     const article_id = req.params.article_id;
+//     console.log("Method Name: ", req.method);
+//     if(req.method == "GET") {
+//         articleModel.articleDetail(article_id, (err, result) => {
+//             res.render('delete_article', { title: "Article Delete", detail: result});
+//         });
+//     }else {
+//         articleModel.deletearticle(id, (err, result) => {
+//             res.redirect('/');
+//         });
+//     }
+// }
+
+
+// to do 
+exports.todo = (req, res) => {
+    const id = req.params.id;
+    articleModel.todo(id, (err, results) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect('/')
+        }
+    });
+}
+
+// process
+exports.process = (req, res) => {
+    const id = req.params.id;
+    articleModel.process(id, (err, results) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect('/')
+        }
+    });
+}
+
+
+// done
+exports.done = (req, res) => {
+    const id = req.params.id;
+    articleModel.done(id, (err, results) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect('/')
+        }
+    });
+}
+
+// delete
+exports.delete = (req, res) => {
+    const id = req.params.id;
+    articleModel.delete(id, (err, results) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect('/');
+        }
+    });
+}
+
+// edit
+exports.edit = (req, res) => {
+    const id = req.params.id;
     if(req.method == "GET") {
-        articleModel.articleDetail(article_id, (err, result) => {
-            res.render('delete_article', { title: "Article Delete", detail: result});
+        articleModel.editForm(id, (err, results) => {
+            if(err){
+                console.log(err);
+            }else{
+                res.render('update_article', { layout : false, article : results[0] } )
+            }
         })
-    }else {
-        articleModel.deletearticle(id, (err, result) => {
-            res.redirect('/article');
+    }else{
+        articleModel.edit(id, (err, results) => {
+            if(err){
+                console.log(err);
+            }else{
+                res.redirect('/');
+            }
         });
     }
 }
-
